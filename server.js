d04d4716175766309c5b058f7aa99f3408182737
Pawel Mazurek <ziurek0@gmail.com>
2014-10-11 17:41:30 +0200

    - weather service ukonczony, prawidłowo cachuje dane, i w ogole jest fajny

--- server.js
+++ server.js
@@ -1,98 +1,34 @@
 var express = require('express');
 var http = require('http');
 var pg = require('pg');
-var fs = require('fs');
 var app = express();
 var server = http.createServer(app);
+var fs = require('fs');
 var html = fs.readFileSync('./index.html');
-var bodyParser = require('body-parser')
-var Forecast = require('forecast');
-var Q = require('Q');
+var bodyParser = require('body-parser');
+
+var weatherService = require('./weatherService');
 
 app.use(bodyParser.json());
 
 var conString = "postgres://bttjzotgxwisqf:6vvG9vGhNRns3RBH7XcoJhqYMc@ec2-184-72-231-67.compute-1.amazonaws.com:5432/daj5aah1m705ur";
 
-// Initialize forecast
-var forecast = new Forecast({
-	service: 'forecast.io',
-	key: '00cedfc6ff3daf7d397f990b4133e3d8',
-	units: 'celcius', // Only the first letter is parsed
-	cache: true,      // Cache API requests?
-	ttl: {            // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/
-		minutes: 27,
-		seconds: 45
-	}
-});
 
-// left most = 14.117
-// right most = 24.133
-// top most = 54.833
-// down most = 49.000
-var leftMost = 14.117;
-var downMost = 49.000;
-var dx = 24.133 - leftMost;
-var dy = 54.833 - downMost;
-
-var coords = [];
-// split Poland territory into mesh
-for( var x=0; x < 2; x++ ) {
-	for(var y=0; y < 2; y++) {
-		var coord = {};
-		// szerokosc geograficzna
-		coord.latitude = downMost + ( dy / 2 ) * y;
-		// dlugosc geograficzna
-		coord.longitude = leftMost + ( dx / 2 ) * x;
-		
-		coords.push( coord );
-	}
-}
-
-var getWeatherJson = function( latitude, longitude ) {
-	var d = Q.defer();
-	forecast.get([coords[i].latitude, coords[i].longitude], true, function(err, weather) {
-		d.resolve( [latitude, longitude, weather] );
-	});
-	return d.promise;
-}
-
-var weatherMap = [];
-
-var qs = [];
-// make api calls for weather
-for( var i=0; i < coords.length; i++ ) {
-	
-	qs.push( getWeatherJson( coords[i].latitude, coords[i].longitude ).then( function( params ) {
-		weatherMap[String(params[0]) + ' ' + String(params[1])] = params[2];
-	}) );
-}
-
-Q.all( qs ).done( function( ) {
-	console.log( 'All data saved' );
-	for( var weatherInfo in weatherMap ) {
-		console.log( weatherMap[weatherInfo] );
-	}
-});
-
-var x, y;
 app.get('/weather/:x/:y', function(req, res) {
-	forecast.get([req.param('x'), req.param('y')], true, function(err, weather) {
-	  // if(err) return console.dir(err);
-		res.send(weather);
-	});
+    res.send( weatherService.getWeather( req.param('x'), req.param('y') ) );
 });
 
 app.get('/', function(req, res) {
     
     // var client = new pg.Client(conString);
-	var client = new pg.Client({
-		user: "bttjzotgxwisqf",
-		password: "6vvG9vGhNRns3RBH7XcoJhqYMc",
-		database: "daj5aah1m705ur",
-		port: 5432,
-		host: "ec2-184-72-231-67.compute-1.amazonaws.com",
-		ssl: true
-	}); 
+    var client = new pg.Client({
+            user: "bttjzotgxwisqf",
+            password: "6vvG9vGhNRns3RBH7XcoJhqYMc",
+            database: "daj5aah1m705ur",
+            port: 5432,
+            host: "ec2-184-72-231-67.compute-1.amazonaws.com",
+            ssl: true
+    }); 
     client.connect();
   
     res.writeHead(200, {'Content-Type': 'text/html'});
---
+++ weatherService.js
@@ -0,0 +1,133 @@
+var Forecast = require('forecast');
+var Q = require('Q');
+var fs = require('fs');
+
+// Initialize forecast
+var forecast = new Forecast({
+    service: 'forecast.io',
+    key: '00cedfc6ff3daf7d397f990b4133e3d8',
+    units: 'celcius', // Only the first letter is parsed
+    cache: true,      // Cache API requests?
+    ttl: {            // How long to cache requests. Uses syntax from moment.js: http://momentjs.com/docs/#/durations/creating/
+        minutes: 27,
+        seconds: 45
+    }
+});
+
+var REFRESH_TIME = ( 4 * 60 * 60 );
+        
+var XX = 10, YY = 10; // na ile pol dzielimy siatke polski
+// left most = 14.117
+// right most = 24.133
+// top most = 54.833
+// down most = 49.000
+var leftMost = 14.117;
+var downMost = 49.000;
+var dx = 24.133 - leftMost;
+var dy = 54.833 - downMost;
+
+var edgeCoordinates = [];
+var weatherMap = [];
+
+var d = Q.defer();
+fs.stat('weather.json', function( err, stat ) {
+    if( err ) {
+        d.resolve( err );
+    } else {
+        var lastModTime = ( new Date() - stat.mtime ) / 1000;
+        d.resolve( lastModTime );
+    }
+});
+
+d.promise.then( function( lastModTime ) {
+    console.log( 'File last modification time: ', lastModTime, 'seconds ago' ); 
+    
+    setTimeout( refreshWeatherInformation, ( REFRESH_TIME - lastModTime ) * 1000 < 0 ? 0 : ( REFRESH_TIME - lastModTime ) * 1000 );
+    
+    // Jezeli dane pogodowe sa aktualne, to tylko je wczytaj z pliku
+    if( lastModTime < REFRESH_TIME ) {
+        fs.readFile('weather.json', 'utf8', function (err, data) {
+            if (err) {
+                return console.log(err);
+            }
+            var weatherTab = JSON.parse(data);
+            
+            edgeCoordinates = [];
+            for( var i=0; i < weatherTab.length; i++ ) {
+                edgeCoordinates.push({ 
+                    latitude: Number(weatherTab[i].latitude),
+                    longitude : Number(weatherTab[i].longitude)
+                });
+                weatherMap[String(weatherTab[0].latitude) + ' ' + String(weatherTab[0].longitude)] = weatherTab[i];
+            }
+        });
+    }
+});
+
+var refreshWeatherInformation = function() {
+    var coords = [];
+    // podziel terytorium Polski na siatke
+    for( var x=0; x < XX; x++ ) {
+        for(var y=0; y < YY; y++) {
+            var coord = {};
+            // szerokosc geograficzna
+            coord.latitude = downMost + ( dy / YY ) * y + ( dy / ( YY * 2 ) );
+            // dlugosc geograficzna
+            coord.longitude = leftMost + ( dx / XX ) * x + ( dx / ( XX * 2 ) );
+
+            coords.push( coord );
+        }
+    }
+
+    var getWeatherJson = function( latitude, longitude ) {
+        var d = Q.defer();
+        forecast.get([coords[i].latitude, coords[i].longitude], true, function(err, weather) {
+            d.resolve( [latitude, longitude, weather] );
+        });
+        return d.promise;
+    };
+
+    var qs = [];
+    edgeCoordinates = [];
+    // make api calls for weather
+    for( var i=0; i < coords.length; i++ ) {
+        qs.push( getWeatherJson( coords[i].latitude, coords[i].longitude ).then( function( params ) {
+            edgeCoordinates.push({ 
+                latitude: params[0],
+                longitude : params[1]
+            });
+            weatherMap[String(params[0]) + ' ' + String(params[1])] = params[2];
+        }) );
+    }
+
+    Q.all( qs ).done( function( ) {
+        var weatherTab = [];
+        for( var weatherInfo in weatherMap ) {
+            weatherTab.push( weatherMap[weatherInfo] );
+        }
+
+        fs.writeFile('weather.json', JSON.stringify(weatherTab, null, 4), function( err, data ) {
+            console.log( 'weather.json saved' );
+        });
+    });
+    setTimeout( refreshWeatherInformation, REFRESH_TIME * 1000 );
+};
+
+exports.getWeather = function( latitude, longitude ) {
+    var found = false;
+    
+    for( var i=0; i < edgeCoordinates.length; i++ ) {
+        if( latitude > ( edgeCoordinates[i].latitude - ( dy / ( YY * 2 ) ) ) && latitude < ( edgeCoordinates[i].latitude + ( dy / ( YY * 2 ) ) ) ) {
+            if( longitude > ( edgeCoordinates[i].longitude - ( dx / ( XX * 2 ) ) ) && longitude < ( edgeCoordinates[i].longitude + ( dx / ( XX * 2 ) ) ) ) {
+                found =  true;
+                return weatherMap[String(edgeCoordinates[i].latitude) + ' ' + String(edgeCoordinates[i].longitude)];
+            }
+        }
+    }
+    if( !found ) {
+        return {
+            state: -1,
+            err: "We do not support weather information in your country. Sorry"
+        };
+    }
+};
